2c1aaa661fd046a296b41446a82e2a9ac616223d
Jmart79 <justinjustinjust111@gmail.com>
2019-03-01 16:41:27 -0700

    Update Hell.java

--- HellManager/src/main/java/gameControllers/Hell.java
+++ HellManager/src/main/java/gameControllers/Hell.java
@@ -7,113 +7,128 @@ import models.Level;
 import models.PowerUp;
 
 public class Hell {
-	
+
 	private DifficultyType difficultyType;
 	private int difficultySoulIncomeMultiplier;
 	private HashMap<Integer, Level> levelManager;
 	private PowerUp[] powerUpArray = new PowerUp[4];
 	private static int totalSoulModifier;
 	private int currentAmountOfSouls;
-	
+
 	public Hell() {
-		
+
 	}
-	private void createLevels(){
-		
+
+	private void createLevels() {
+
 		int levelNumber;
 		int maxSoulCap = 10000;
 		int currentSoulAmount = getCurrentAmountOfSouls();
-		
-		for(int i =0; i < 7; i++){
+
+		for (int i = 0; i < 7; i++) {
 			levelNumber = i + 1;
 			Level l = new Level(levelNumber, maxSoulCap, currentSoulAmount);
-			levelManager.put(levelNumber,l);
+			levelManager.put(levelNumber, l);
 		}
 	}
-	
+
 	private boolean levelCapacityManager(int levelNumber) {
 		boolean maxMet = false;
 		Level l = levelManager.get(levelNumber);
-		if(l.getCurrentSoulAmount() >= l.getMaxSoulCap()){
+		if (l.getCurrentSoulAmount() >= l.getMaxSoulCap()) {
 			l.setCurrentSoulAmount(l.getMaxSoulCap());
 			maxMet = true;
 		}
 		return maxMet;
 	}
-	
+
 	private String displayerLevelMaxCapacity(int levelNumber) {
 		StringBuilder maxCapacity = new StringBuilder();
 		Level l = levelManager.get(levelNumber);
 		maxCapacity.append(l.getMaxSoulCap());
-		
+
 		return maxCapacity.toString();
 	}
-	
+
 	private String displayLevelCurrentCapacity(int levelNumber) {
-		
+
 		StringBuilder currentCapacity = new StringBuilder();
 		Level l = levelManager.get(levelNumber);
 		currentCapacity.append(l.getCurrentSoulAmount());
-		
+
 		return currentCapacity.toString();
-		
+
 	}
-	
+
 	private void maxCapModifier() {
 		boolean isFilled;
 		int passiveMod = 0;
 		Level l = null;
-		for(int i = 0; i < 7; i++){
-			isFilled = levelCapacityManager((i+1));
-			if(isFilled){
-				l = levelManager.get(i+1);
-				passiveMod  +=  l.getMaxSoulCapModifier();
+		for (int i = 0; i < 7; i++) {
+			isFilled = levelCapacityManager((i + 1));
+			if (isFilled) {
+				l = levelManager.get(i + 1);
+				passiveMod += l.getMaxSoulCapModifier();
 				modifierModifier(passiveMod);
 			}
 		}
 	}
-	
+
 	private void createPowerUps() {
 		int baseSoulFee = 0;
 		int soulFee;
 		int powerUpTier;
-		
-		
-		for(int i = 0; i < 4; i++){
-			powerUpTier = i +1;
-		if(i == 0){
-			baseSoulFee = 2500;
-			PowerUp pu = new PowerUp(baseSoulFee, powerUpTier);
-			powerUpArray[i] = pu;
-		}else{
 
-			int temp = baseSoulFee * i+1;
-			soulFee = baseSoulFee + temp;
-			PowerUp pu = new PowerUp(soulFee, powerUpTier);
-			powerUpArray[i] = pu;
+		for (int i = 0; i < 4; i++) {
+			powerUpTier = i + 1;
+			if (i == 0) {
+				baseSoulFee = 2500;
+				PowerUp pu = new PowerUp(baseSoulFee, powerUpTier);
+				powerUpArray[i] = pu;
+			} else {
+
+				int temp = baseSoulFee * i + 1;
+				soulFee = baseSoulFee + temp;
+				PowerUp pu = new PowerUp(soulFee, powerUpTier);
+				powerUpArray[i] = pu;
 			}
 		}
 	}
+
 	private void powerUpManager() {
 		int tierOneCounter;
 		int tierTwoCounter;
 		int tierThreeCounter;
 		int tierFourCounter;
-		
-		
+
+		tierOneCounter = 0;
+		tierTwoCounter = 0;
+		tierThreeCounter = 0;
+		tierFourCounter = 0;
+
+		if (tierOneCounter < 4) {
+
+		}
+		if (tierTwoCounter < 4) {
+
+		}
+		if (tierThreeCounter < 4) {
+
+		}
+		if (tierFourCounter < 4) {
+
+		}
+
 	}
-	
-	
+
 	private void powerUpUpgrader() {
-		
-		
-		
+
 	}
-	
+
 	private void modifierModifier(int modifier) {
-		
+
 		totalSoulModifier += modifier;
-		
+
 	}
 
 	public DifficultyType getDifficultyType() {
@@ -164,5 +179,4 @@ public class Hell {
 		Hell.totalSoulModifier = totalSoulModifier;
 	}
 
-	
 }
