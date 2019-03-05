8587a35c9992cf413b57863ec69537de41a02201
SirRolls <rockyjaymc@gmail.com>
2019-03-05 15:04:51 -0700

    Updated GUI

--- HellManagerGUI/src/application/Main.java
+++ HellManagerGUI/src/application/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			Parent root = FXMLLoader.load(getClass().getResource("scene.fxml"));
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root,1280,800);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
