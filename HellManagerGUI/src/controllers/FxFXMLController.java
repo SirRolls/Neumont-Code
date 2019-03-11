d9f3e8b5e9745960713309db9f325bfadabe835e
SirRolls <rockyjaymc@gmail.com>
2019-03-11 16:53:27 -0600

    GUI Updated
    Added ActionEvents to CBs

--- HellManagerGUI/src/controllers/FxFXMLController.java
+++ HellManagerGUI/src/controllers/FxFXMLController.java
@@ -1,10 +1,13 @@
 package controllers;
 
 import java.util.ResourceBundle;
-import java.awt.Button;
+
 import java.net.URL;
 
 import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ProgressBar;
@@ -31,44 +34,108 @@ public class FxFXMLController {
 	    @FXML
 	    private ProgressBar pb9;
 	    @FXML
-	    private ChoiceBox Layer1CB;
+	    private ChoiceBox<Integer> Layer1CB;
 	    @FXML
-	    private ChoiceBox Layer2CB;
+	    private ChoiceBox<Integer> Layer2CB;
 	    @FXML
-	    private ChoiceBox Layer3CB;
+	    private ChoiceBox<Integer> Layer3CB;
 	    @FXML
-	    private ChoiceBox Layer4CB;
+	    private ChoiceBox<Integer> Layer4CB;
 	    @FXML
-	    private ChoiceBox Layer5CB;
+	    private ChoiceBox<Integer> Layer5CB;
 	    @FXML
-	    private ChoiceBox Layer6CB;
+	    private ChoiceBox<Integer> Layer6CB;
 	    @FXML
-	    private ChoiceBox Layer7CB;
+	    private ChoiceBox<Integer> Layer7CB;
 	    @FXML
 	    private ResourceBundle resources;
-	    @FXML
-	    private Button startButton;
 	     
 	    // Add a public no-args constructor
-	    public FxFXMLController(){
+	    public FxFXMLController() 
+	    {
 	    }
 	     
 	    @FXML
-	    private void initialize(){
+	    private void initialize() 
+	    {
 	    }
 	    @FXML
 	    public void Layer1ButtonPressed() {
 	    	
 	    }
 	    @FXML
-	    public void InitGame() {
+	    public void Start() {
 	    	Layer1CB.setItems(FXCollections.observableArrayList(10,50,100,1000));
+	    	Layer1CB.setOnAction(new EventHandler<ActionEvent>() {
+				
+				@Override
+				public void handle(ActionEvent event) {
+					int choice = Layer1CB.getSelectionModel().getSelectedItem();
+					
+				}
+			});
 	    	Layer2CB.setItems(FXCollections.observableArrayList(10,50,100,1000));
-	    	Layer3CB.setItems(FXCollections.observableArrayList(10,50,100,1000));
-	    	Layer4CB.setItems(FXCollections.observableArrayList(10,50,100,1000));
-	    	Layer5CB.setItems(FXCollections.observableArrayList(10,50,100,1000));
-	    	Layer6CB.setItems(FXCollections.observableArrayList(10,50,100,1000));
-	    	Layer7CB.setItems(FXCollections.observableArrayList(10,50,100,1000));
+	    	Layer2CB.setOnAction(new EventHandler<ActionEvent>() {
+	    		
+	    		@Override
+	    		public void handle(ActionEvent event) {
+					int choice = Layer2CB.getSelectionModel().getSelectedItem();
+					
+				}}
 	    	
+	    	);
+	    	Layer3CB.setItems(FXCollections.observableArrayList(10,50,100,1000));
+	    	Layer3CB.setOnAction(new EventHandler<ActionEvent>() {
+	    		
+	    		@Override
+	    		public void handle(ActionEvent event) {
+					int choice = Layer3CB.getSelectionModel().getSelectedItem();
+					
+				}}
+	    	
+	    	);
+	    	Layer4CB.setItems(FXCollections.observableArrayList(10,50,100,1000));
+	    	Layer4CB.setOnAction(new EventHandler<ActionEvent>() {
+	    		
+	    		@Override
+	    		public void handle(ActionEvent event) {
+					int choice = Layer4CB.getSelectionModel().getSelectedItem();
+					
+				}}
+	    	
+	    	);
+	    	Layer5CB.setItems(FXCollections.observableArrayList(10,50,100,1000));
+	    	Layer5CB.setOnAction(new EventHandler<ActionEvent>() {
+	    		
+	    		@Override
+	    		public void handle(ActionEvent event) {
+					int choice = Layer5CB.getSelectionModel().getSelectedItem();
+					
+				}}
+	    	
+	    	);
+	    	Layer6CB.setItems(FXCollections.observableArrayList(10,50,100,1000));
+	    	Layer6CB.setOnAction(new EventHandler<ActionEvent>() {
+	    		
+	    		@Override
+	    		public void handle(ActionEvent event) {
+					int choice = Layer6CB.getSelectionModel().getSelectedItem();
+					
+				}}
+	    	
+	    	);
+	    	Layer7CB.setItems(FXCollections.observableArrayList(10,50,100,1000));
+	    	Layer7CB.setOnAction(new EventHandler<ActionEvent>() {
+	    		
+	    		@Override
+	    		public void handle(ActionEvent event) {
+					int choice = Layer7CB.getSelectionModel().getSelectedItem();
+					
+				}}
+	    	
+	    	);
 	    	}
+		public void Exit() {
+	    	System.exit(0);
+	    }
 }
